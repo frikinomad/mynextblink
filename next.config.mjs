b82af34b40566b6c49e6340660e531e6e218c8f2
Rishi Chaturvedi <rishichaturvedi0012@gmai.com>
2024-08-24 12:59:21 +0530

    update connection security policy in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' data:;
+  connect-src 'self' https://api.dscvr.one https://api1.stg.dscvr.one https://*.helius-rpc.com https://proxy.dial.to;
+`;
 
-export default nextConfig;
+export default {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+    ];
+  },
+};
